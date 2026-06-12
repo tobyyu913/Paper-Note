96ce01847f0dda118fc8909aa8044627c72ff33d
tobyyu913 <tobyyu913@gmail.com>
2026-06-12 17:11:21 +0800

    Initial Commit

---
+++ Paper Note/Paper_NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Paper_NoteApp.swift
+//  Paper Note
+//
+//  Created by Toby Yu on 12/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Paper_NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
